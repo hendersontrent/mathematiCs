258df6104a7534bf1eecf169ada0982df020100d
Trent Henderson <trent.henderson1@outlook.com>
2022-02-24 23:33:49 +1100

    Initial upload

---
+++ src/linalg.h
@@ -0,0 +1,9 @@
+#ifndef LINALG
+#define LINALG
+
+extern const double vector_add(const double x[], const double y[]);
+extern const double vector_sub(const double x[], const double y[]);
+extern const double vector_scalar_mult(const double x[], const double y[]);
+extern double dot_prod(const double x[], const double y[]);
+
+#endif
